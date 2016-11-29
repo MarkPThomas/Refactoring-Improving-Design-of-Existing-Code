5d40a0b5bf806ca8235a549a9bc94f0fa6b16a54
MarkPThomas <markums@gmail.com>
2016-11-29 10:17:37 -0800

    First commit
    Unit tests all running in the green before performing an example
    refactoring.

---
+++ 1- A First Example/Video Store (1)/src/VideoStore.Core/Customer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoStore.Core
+{
+    /// <summary>
+    /// Represents a customer of the store.
+    /// </summary>
+    public class Customer
+    {
+        public string Name { get; private set; }
+
+        private List<Rental> _rentals = new List<Rental>();
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+
+        public void AddRental(Rental arg)
+        {
+            _rentals.Add(arg);
+        }
+
+        public string Statement()
+        {
+            double totalAmount = 0;
+            int frequentRenterPoints = 0;
+            string result = "Rental Record for " + Name + "\n";
+            foreach (Rental each in _rentals)
+            {
+                double thisAmount = 0;
+
+                // Determine amounts for each line.
+                switch (each.Movie.PriceCode)
+                {
+                    case Movie.REGULAR:
+                        thisAmount += 2;
+                        if (each.DaysRented > 2)
+                            thisAmount += (each.DaysRented - 2) * 1.5;
+                        break;
+                    case Movie.NEW_RELEASE:
+                        thisAmount += each.DaysRented * 3;
+                        break;
+                    case Movie.CHILDRENS:
+                        thisAmount += 1.5;
+                        if (each.DaysRented > 3)
+                            thisAmount += (each.DaysRented - 3) * 1.5;
+                        break;
+                }
+
+                // Add frequent renters points.
+                frequentRenterPoints++;
+
+                // Add bonus for a two day new release rental
+                if ((each.Movie.PriceCode == Movie.NEW_RELEASE) &&
+                        each.DaysRented > 1)
+                    frequentRenterPoints++;
+
+                // Show figures for this rental.
+                result += "\t" + each.Movie.Title + "\t" + thisAmount.ToString() + "\n";
+                totalAmount += thisAmount;
+            }
+
+            // Add footer lines.
+            result += "Amount owed is " + totalAmount.ToString() + "\n";
+            result += "You earned " + frequentRenterPoints.ToString() + " frequent renter points";
+            return result;
+        }
+    }
+}
